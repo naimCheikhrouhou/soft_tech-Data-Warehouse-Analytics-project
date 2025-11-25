6087fd438492301a8b30c5aadc5ef71a8f3eccfd
hassengrine <151055497+hassengrine@users.noreply.github.com>
2025-11-25 20:41:58 +0000

    Update DDL script.sql

--- scripts/silver layer/DDL script.sql
+++ scripts/silver layer/DDL script.sql
@@ -2,7 +2,7 @@
 -- 1. TABLES CLIENTS / SALES
 -- ========================================
 
-CREATE TABLE customers (
+CREATE TABLE silver.customers (
     customer_id INT PRIMARY KEY,
     company_name VARCHAR(100) NOT NULL,
     contact_name VARCHAR(100),
@@ -17,7 +17,7 @@ CREATE TABLE customers (
     source  VARCHAR(50) DEFAULT 'dataset/buisiness_data/customers.csv/bronze.customers'
 );
 
-CREATE TABLE products (
+CREATE TABLE silver.products (
     product_id INT PRIMARY KEY,
     product_name VARCHAR(100) NOT NULL,
     category VARCHAR(50),
@@ -31,10 +31,10 @@ CREATE TABLE products (
     source  VARCHAR(50) DEFAULT 'dataset/buisiness_data/products.csv/bronze.products'
 );
 
-CREATE TABLE sales_orders (
+CREATE TABLE silver.sales_orders (
     sale_id INT PRIMARY KEY,
-    customer_id INT REFERENCES customers(customer_id),
-    product_id INT REFERENCES products(product_id),
+    customer_id INT REFERENCES silver.customers(customer_id),
+    product_id INT REFERENCES silver.products(product_id),
     quantity INT,
     total_price DECIMAL(10,2),
     payment_method VARCHAR(50),
@@ -46,59 +46,24 @@ CREATE TABLE sales_orders (
 );
 
 -- ========================================
--- 2. TABLES PROJETS / BUGS
+-- 2. TABLES Teams/Employees/ PROJETS / BUGS
 -- ========================================
-
-CREATE TABLE projects (
-    project_id INT PRIMARY KEY,
-    customer_id INT REFERENCES customers(customer_id),
-    project_name VARCHAR(100),
-    start_date DATE,
-    end_date DATE,
-    delivered_date DATE,
-    project_manager INT,
-    estimated_cost DECIMAL(10,2),
-    actual_cost DECIMAL(10,2),
-    status VARCHAR(20),
-                                                                   --meta data
-    record_created_at DATETIME DEFAULT GETDATE(),
-    record_created_by VARCHAR(50) DEFAULT SYSTEM_USER,
-    source  VARCHAR(50) DEFAULT 'dataset/buisiness_data/projects.csv/bronze.customers'
-);
-
-CREATE TABLE project_bug_reports (
-    bug_id INT PRIMARY KEY,
-    project_id INT REFERENCES projects(project_id),
-    severity VARCHAR(20),
-    description TEXT,
-    reported_date DATE,
-    resolved BOOLEAN,
-                                                                   --meta data
-    record_created_at DATETIME DEFAULT GETDATE(),
-    record_created_by VARCHAR(50) DEFAULT SYSTEM_USER,
-    source  VARCHAR(50) DEFAULT 'dataset/buisiness_data/project_bug_reports.csv/bronze.customers'
-);
-
--- ========================================
--- 3. TABLES EMPLOYEES / TEAMS / PERFORMANCE
--- ========================================
-
-CREATE TABLE teams (
+CREATE TABLE silver.teams (
     team_id INT PRIMARY KEY,
     team_name VARCHAR(50),
     department VARCHAR(50),
-    manager_id INT,
+    manager_id INT ,
                                                                    --meta data
     record_created_at DATETIME DEFAULT GETDATE(),
     record_created_by VARCHAR(50) DEFAULT SYSTEM_USER,
     source  VARCHAR(50) DEFAULT 'dataset/employees&condidate/teams.csv/bronze.customers'
 );
 
-CREATE TABLE employees (
+CREATE TABLE silver.employees (
     employee_id INT PRIMARY KEY,
     full_name VARCHAR(100),
     role VARCHAR(50),
-    team_id INT REFERENCES teams(team_id),
+    team_id INT REFERENCES silver.teams(team_id),
     hire_date DATE,
     salary DECIMAL(10,2),
     seniority VARCHAR(20),
@@ -108,16 +73,52 @@ CREATE TABLE employees (
     record_created_by VARCHAR(50) DEFAULT SYSTEM_USER,
     source  VARCHAR(50) DEFAULT 'dataset/employees&condidate/employees.csv/bronze.customers'
 );
+CREATE TABLE silver.projects (
+    project_id INT PRIMARY KEY,
+    customer_id INT REFERENCES silver.customers(customer_id),
+    project_name VARCHAR(100),
+    start_date DATE,
+    end_date DATE,
+    delivered_date DATE,
+    project_manager_id INT REFERENCES silver.employees(employee_id),
+    estimated_cost DECIMAL(10,2),
+    actual_cost DECIMAL(10,2),
+    status VARCHAR(20),
+                                                                   --meta data
+    record_created_at DATETIME DEFAULT GETDATE(),
+    record_created_by VARCHAR(50) DEFAULT SYSTEM_USER,
+    source  VARCHAR(50) DEFAULT 'dataset/buisiness_data/projects.csv/bronze.customers'
+);
 
-CREATE TABLE employee_performance (
+CREATE TABLE silver.project_bug_reports (
+    bug_id INT PRIMARY KEY,
+    project_id INT REFERENCES silver.projects(project_id),
+    severity VARCHAR(20),
+    description TEXT,
+    reported_date DATE,
+    resolved bit,
+                                                                   --meta data
+    record_created_at DATETIME DEFAULT GETDATE(),
+    record_created_by VARCHAR(50) DEFAULT SYSTEM_USER,
+    source  VARCHAR(50) DEFAULT 'dataset/buisiness_data/project_bug_reports.csv/bronze.customers'
+);
+
+-- ========================================
+-- 3. TABLES PERFORMANCE
+-- ========================================
+
+
+
+
+CREATE TABLE silver.employee_performance (
     perf_id INT PRIMARY KEY,
-    employee_id INT REFERENCES employees(employee_id),
-    month INT,
-    year INT,
+    employee_id INT REFERENCES silver.employees(employee_id),
+    start_date date,
+    end_date date,
     tasks_completed INT,
     overtime_hours INT,
     performance_score INT,
-    project_success_rate DECIMAL(5,2) 
+    project_success_rate DECIMAL(5,2) ,
                                                                   --meta data
     record_created_at DATETIME DEFAULT GETDATE(),
     record_created_by VARCHAR(50) DEFAULT SYSTEM_USER,
@@ -128,7 +129,7 @@ CREATE TABLE employee_performance (
 -- 4. TABLES RECRUTEMENT / CANDIDATS
 -- ========================================
 
-CREATE TABLE candidates (
+CREATE TABLE silver.candidates (
     candidate_id INT PRIMARY KEY,
     full_name VARCHAR(100),
     email VARCHAR(100),
@@ -144,9 +145,9 @@ CREATE TABLE candidates (
     source  VARCHAR(50) DEFAULT 'dataset/employees&condidate/candidates.csv/bronze.customers'
 );
 
-CREATE TABLE candidate_cv_raw (
+CREATE TABLE silver.candidate_cv_raw (
     cv_id INT PRIMARY KEY,
-    candidate_id INT REFERENCES candidates(candidate_id),
+    candidate_id INT REFERENCES silver.candidates(candidate_id),
     cv_text TEXT,
     parsed_keywords TEXT,
     language_detected VARCHAR(20),
@@ -157,10 +158,10 @@ CREATE TABLE candidate_cv_raw (
     source  VARCHAR(50) DEFAULT 'dataset/employees&condidate/candidate_cv_raw./bronze.customers'
 );
 
-CREATE TABLE candidate_interviews (
+CREATE TABLE silver.candidate_interviews (
     interview_id INT PRIMARY KEY,
-    candidate_id INT REFERENCES candidates(candidate_id),
-    recruiter_id INT REFERENCES employees(employee_id),
+    candidate_id INT REFERENCES silver.candidates(candidate_id),
+    recruiter_id INT REFERENCES silver.employees(employee_id),
     interview_score INT,
     notes TEXT,
     stage VARCHAR(50),
@@ -170,5 +171,8 @@ CREATE TABLE candidate_interviews (
     record_created_by VARCHAR(50) DEFAULT SYSTEM_USER,
     source  VARCHAR(50) DEFAULT 'dataset/employees&condidate/candidate_interviews.csv/bronze.customers'
 );
-
+ALTER TABLE silver.teams
+ADD CONSTRAINT fk_team_leader
+FOREIGN KEY (manager_id)
+REFERENCES silver.employees(employee_id);
 
