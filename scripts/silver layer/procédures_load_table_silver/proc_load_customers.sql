0ae8adca5359d72dcd236dc929ebc787e965d578
naimCheikhrouhou <150935235+naimCheikhrouhou@users.noreply.github.com>
2025-11-28 16:36:37 +0100

    Update proc_load_customers.sql

--- scripts/silver layer/procédures_load_table_silver/proc_load_customers.sql
+++ scripts/silver layer/procédures_load_table_silver/proc_load_customers.sql
@@ -30,15 +30,15 @@ BEGIN
     END AS formatted_email,
            
     CASE 
-        WHEN LOWER(TRIM(country)) = 'tunisie' THEN '+216' + RIGHT(TRIM(ISNULL(phone,'n/a')), 8)
-        WHEN LOWER(TRIM(country)) = 'france' THEN '+33'  + RIGHT(TRIM(ISNULL(phone,'n/a')), 8)
-        WHEN LOWER(TRIM(country)) = 'espagne' THEN '+34' + RIGHT(TRIM(ISNULL(phone,'n/a')), 8)
-        WHEN LOWER(TRIM(country)) = 'allemagne' THEN '+49' + RIGHT(TRIM(ISNULL(phone,'n/a')), 8)
-        WHEN LOWER(TRIM(country)) = 'italie' THEN '+39' + RIGHT(TRIM(ISNULL(phone,'n/a')), 8)
-        WHEN LOWER(TRIM(country)) = 'japon' THEN '+81' + RIGHT(TRIM(ISNULL(phone,'n/a')), 8)
-        WHEN LOWER(TRIM(country)) = 'canada' THEN '+1' + RIGHT(TRIM(ISNULL(phone,'n/a')), 8)
-        WHEN LOWER(TRIM(country)) = 'usa' THEN '+1' + RIGHT(TRIM(ISNULL(phone,'n/a')), 8)
-        WHEN LOWER(TRIM(country)) = 'maroc' THEN '+212' + RIGHT(TRIM(ISNULL(phone,'n/a')), 8)
+        WHEN LOWER(TRIM(country)) = 'tunisie' THEN '+216' + RIGHT(TRIM(ISNULL(phone,' n/a')), 8)
+        WHEN LOWER(TRIM(country)) = 'france' THEN '+33'  + RIGHT(TRIM(ISNULL(phone,' n/a')), 8)
+        WHEN LOWER(TRIM(country)) = 'espagne' THEN '+34' + RIGHT(TRIM(ISNULL(phone,' n/a')), 8)
+        WHEN LOWER(TRIM(country)) = 'allemagne' THEN '+49' + RIGHT(TRIM(ISNULL(phone,' n/a')), 8)
+        WHEN LOWER(TRIM(country)) = 'italie' THEN '+39' + RIGHT(TRIM(ISNULL(phone,' n/a')), 8)
+        WHEN LOWER(TRIM(country)) = 'japon' THEN '+81' + RIGHT(TRIM(ISNULL(phone,' n/a')), 8)
+        WHEN LOWER(TRIM(country)) = 'canada' THEN '+1' + RIGHT(TRIM(ISNULL(phone,' n/a')), 8)
+        WHEN LOWER(TRIM(country)) = 'usa' THEN '+1' + RIGHT(TRIM(ISNULL(phone,' n/a')), 8)
+        WHEN LOWER(TRIM(country)) = 'maroc' THEN '+212' + RIGHT(TRIM(ISNULL(phone,' n/a')), 8)
        ELSE RIGHT(TRIM(phone), 8)                               -- default: just last 8 digits
                                            END AS phone,
 
