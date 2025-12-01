caf3003e3301e5ad2e615bbd8fed41d89173abcf
hassengrine <151055497+hassengrine@users.noreply.github.com>
2025-12-01 15:55:04 +0000

    Update and rename load_gold.sql to load_dim_gold.sql

--- scripts/gold layer/load_dim_gold.sql
+++ scripts/gold layer/load_dim_gold.sql
@@ -1,6 +1,4 @@
-
-
-truncate table gold.dim_customers;
+delete from gold.dim_customers;
 INSERT INTO gold.dim_customers(customer_id, company_name, country, created_at)
 SELECT 
     customer_id,
@@ -8,26 +6,34 @@ SELECT
     country,
     created_at
 FROM silver.customers;
-truncate table gold.dim_product;
+
+
+delete from gold.dim_product;
 INSERT INTO gold.dim_product(product_id, product_name, category, base_price, development_hours, version)
 SELECT 
     product_id, product_name, category, base_price, development_hours, version
 FROM silver.products;
-truncate table gold.dim_project;
+
+
+delete from gold.dim_project;
 INSERT INTO gold.dim_project(project_id, project_name, status)
 SELECT project_id, project_name, status
 FROM silver.projects;
-truncate table gold.dim_employee;
+
+
+delete from gold.dim_employee;
 INSERT INTO gold.dim_employee(employee_id, full_name, role, hire_date, salary, seniority, email, team_id)
 SELECT 
     employee_id, full_name, role, hire_date, salary, seniority, email, team_id
 FROM silver.employees;
-truncate table gold.dim_team;
+
+
+delete from gold.dim_team;
 INSERT INTO gold.dim_team(team_id, team_name, department, manager_team_id)
 SELECT 
     team_id, team_name, department, manager_id
 FROM silver.teams;
-TRUNCATE TABLE gold.dim_candidates;
+delete from gold.dim_candidates;
 
 INSERT INTO gold.dim_candidates
 (
@@ -60,7 +66,7 @@ SELECT
 
     /* ====================================================
        FUSION DÉDOUBLONNÉE DU CV TEXTE
-       STRING_SPLIT ne fonctionne pas avec CRLF → on remplace par ' '
+       STRING_SPLIT ne fonctionne pas avec CRLF ? on remplace par ' '
     ==================================================== */
     (
         SELECT STRING_AGG(value, CHAR(13) + CHAR(10))
@@ -116,67 +122,3 @@ LEFT JOIN silver.candidate_cv_raw cv1
 LEFT JOIN silver.candidate_cv_raw_linkedin cv2
     ON c.candidate_id = cv2.candidate_id;
 
-
-    TRUNCATE TABLE gold.fact_products_sales;
-INSERT INTO gold.fact_products_sales(product_key, customer_key, sale_date_key, quantity, total_price)
-SELECT 
-    dp.product_key,
-    dc.customer_key,
-    CONVERT(INT, FORMAT(so.sale_date, 'yyyyMMdd')),
-    so.quantity,
-    so.total_price
-FROM silver.sales_orders so
-JOIN gold.dim_product dp ON dp.product_id = so.product_id
-JOIN gold.dim_customers dc ON dc.customer_id = so.customer_id;
-TRUNCATE TABLE gold.fact_project_suivis;
-INSERT INTO gold.fact_project_suivis
-(
-    project_key, customer_id, manager_project_id,
-    start_date_key, end_date_key, delivered_date_key,
-    estimated_cost, actual_cost
-)
-SELECT 
-    dp.project_key,
-    p.customer_id,
-    p.project_manager_id,
-    CONVERT(INT, FORMAT(p.start_date, 'yyyyMMdd')),
-    CONVERT(INT, FORMAT(p.end_date, 'yyyyMMdd')),
-    CONVERT(INT, FORMAT(p.delivered_date, 'yyyyMMdd')),
-    p.estimated_cost,
-    p.actual_cost
-FROM silver.projects p
-JOIN gold.dim_project dp ON dp.project_id = p.project_id;
-
-TRUNCATE TABLE gold.fact_employee_performance;
-INSERT INTO gold.fact_employee_performance
-(
-    employee_key, start_date_key, end_date_key,
-    tasks_completed, overtime_hours, performance_score, project_success_rate
-)
-SELECT 
-    de.employee_key,
-    CONVERT(INT, FORMAT(ep.start_date, 'yyyyMMdd')),
-    CONVERT(INT, FORMAT(ep.end_date, 'yyyyMMdd')),
-    ep.tasks_completed,
-    ep.overtime_hours,
-    ep.performance_score,
-    ep.project_success_rate
-FROM silver.employee_performance ep
-JOIN gold.dim_employee de ON de.employee_id = ep.employee_id;
-
-TRUNCATE TABLE gold.fact_interview;
-INSERT INTO gold.fact_interview
-(
-    candidate_key, employee_key, team_id,
-    interview_date_key, interview_score, result
-)
-SELECT 
-    dc.candidate_key,
-    de.employee_key,
-    ci.team_id,
-    CONVERT(INT, FORMAT(CAST(GETDATE() AS DATE), 'yyyyMMdd')),
-    ci.interview_score,
-    ci.result
-FROM silver.candidate_interviews ci
-JOIN gold.dim_candidates dc ON dc.candidate_id = ci.candidate_id
-JOIN gold.dim_employee de ON de.employee_id = ci.recruiter_id;
