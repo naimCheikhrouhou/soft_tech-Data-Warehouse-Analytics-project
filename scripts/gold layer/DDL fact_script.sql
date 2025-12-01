e37184046008f7464e65d8f2bc4177b27a15fdae
hassengrine <151055497+hassengrine@users.noreply.github.com>
2025-12-01 11:01:44 +0000

    Create DDL fact_script.sql

---
+++ scripts/gold layer/DDL fact_script.sql
@@ -0,0 +1,57 @@
+
+print('gold.fact_products_sales');
+print('====================================================================');
+drop table if exists gold.fact_products_sales;
+CREATE TABLE gold.fact_products_sales (
+    product_key INT references gold.dim_product(product_key) on delete cascade,
+    customer_key INT references gold.dim_customers(customer_key)on delete cascade,
+    sale_date_key INT references gold.dim_date(date_key)on delete cascade,
+    quantity INT ,
+    total_price DECIMAL(10,2)
+);
+
+print('gold.fact_project_suivis');
+print('====================================================================');
+
+drop table if exists gold.fact_project_suivis;
+CREATE TABLE gold.fact_project_suivis (
+    project_key INT references gold.dim_project(project_key) on delete cascade,
+    customer_id INT references gold.dim_customers(customer_key) on delete cascade,
+    manager_project_id INT references gold.dim_employee(employee_key) on delete cascade,
+    start_date_key INT references gold.dim_date(date_key) on delete cascade,
+    end_date_key INT references gold.dim_date(date_key) on delete cascade,
+    delivered_date_key INT references gold.dim_date(date_key) on delete cascade,
+    estimated_cost DECIMAL(10,2),
+    actual_cost DECIMAL(10,2)
+);
+
+
+print('gold.fact_employee_performance');
+print('====================================================================');
+
+drop table if exists gold.fact_employee_performance;
+CREATE TABLE gold.fact_employee_performance (
+    employee_key INT references gold.dim_employee(employee_key) on delete cascade,
+    start_date_key INT references gold.dim_date(date_key) on delete cascade,
+    end_date_key INT references gold.dim_date(date_key) on delete cascade,
+    tasks_completed INT,
+    overtime_hours DECIMAL(10,2),
+    performance_score INT,
+    project_success_rate DECIMAL(5,2)
+);
+
+
+
+
+print('gold.fact_interview');
+print('====================================================================');
+
+drop table if exists gold.fact_interview;
+CREATE TABLE gold.fact_interview (
+    candidate_key INT references gold.dim_candidates(candidate_key) on delete cascade,
+    employee_key INT references gold.dim_employee(employee_key) on delete cascade,
+    team_id INT references gold.dim_team(team_key) on delete cascade,
+    interview_date_key INT references gold.dim_date(date_key) on delete cascade,
+    interview_score INT,
+    result VARCHAR(50)
+);
