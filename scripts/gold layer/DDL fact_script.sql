9b568e16bf5cd8f86749b955c067103a62317325
naimCheikhrouhou <150935235+naimCheikhrouhou@users.noreply.github.com>
2025-12-02 23:55:05 +0100

    Update DDL fact_script.sql

--- scripts/gold layer/DDL fact_script.sql
+++ scripts/gold layer/DDL fact_script.sql
@@ -37,6 +37,7 @@ drop table if exists gold.fact_employee_performance;
 CREATE TABLE gold.fact_employee_performance (
     employee_key INT references gold.dim_employee(employee_key) on delete cascade,
     project_key  INT references gold.dim_project(project_key) on delete cascade,
+    team_key     INT references gold.dim_team(team_key)      on delete cascade,
     start_date_key INT references gold.dim_date(date_key) on delete cascade,
     end_date_key INT references gold.dim_date(date_key) on delete NO ACTION,
     tasks_completed INT,
